ec9672c1696352e08658e92dda276075ced29996
Egor Schurkov <sch.egor@gmail.com>
2017-01-03 22:33:50 +0300

    Добавил в функцию формирования меню вывод того, что "будем готовить". Изменил код выбора действий пользователя.

--- Homework_2.1/cookbook.py
+++ Homework_2.1/cookbook.py
@@ -10,18 +10,19 @@ def read_cookbook(cookbook_filename):
 	return cookbook
 
 def get_menu(cookbook):
-	menu = []
+	menu = {}
 	dishes = [dish.strip() for dish in input('Составьте меню. Введите названия блюд через запятую: ').split(',')]
 	for dish in dishes:
 		if cookbook.get(dish.capitalize()):
-			menu.append(cookbook[dish.capitalize()])
+			menu[dish.capitalize()] = cookbook[dish.capitalize()]
 		else:
-			print('Блюдо "%s" отсутствует в кулинарной книге. Исключено из меню.' % dish)
+			print('Блюдо "%s" отсутствует в кулинарной книге. Исключено из меню.' % dish)	
+	print('Будем готовить:',', '.join(list(dishname for dishname in menu)))
 	return menu
 
 def get_shop_list_by_menu(menu, people_count):
 	shop_list = {}
-	for dish in menu:
+	for name, dish in menu.items():
 		for ingridient in dish:
 			ingridient['quantity'] = int(ingridient['quantity']) * people_count
 			if ingridient['product'] in shop_list:
@@ -48,13 +49,7 @@ def print_cookbook():
 		print(key)
 		print("".join(list(('{product} {quantity} {unit}\n'.format(**ingridient) for ingridient in cookbook_item))))
 
+functions = {'1': print_cookbook, '2': create_shop_list}
 while True:
-	user_input = input('Выберите действие:\n1 - Вывести кулинарную книгу.\n2 - Составить меню и список покупок.\n0 - Выход\n=> ')
-	if user_input == '1':
-		print_cookbook()
-	elif user_input == '2':
-		create_shop_list()
-	elif user_input == '0':
-		break
-	else:
-		print('\nОшибка ввода, попробуйте еще раз.')
+	user_input = input('Выберите действие:\n1 - Вывести кулинарную книгу.\n2 - Составить меню и список покупок.\nДля выхода введите любой другой символ.\n=> ')
+	functions.get(user_input, exit)()
