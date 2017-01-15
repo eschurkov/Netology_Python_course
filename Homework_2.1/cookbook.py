df9c37a70135f18ce17b54a1f5e7ded65ca0a1b1
Egor Schurkov <sch.egor@gmail.com>
2017-01-15 18:10:28 +0300

    Добавлена возвожмност работы с файлами json и yaml

--- Homework_2.1/cookbook.py
+++ Homework_2.1/cookbook.py
@@ -1,12 +1,31 @@
+import json
+import yaml
+
+# для генерации файла json из txt
+def dump_json (jsonfilename = 'cookbook.json'):
+	with open(jsonfilename, 'w', encoding='utf-8') as f:
+		txtcookbook = read_cookbook('cookbook.txt')
+		jsoncookbook = json.dump(txtcookbook, f)
+
+# для генерации файла yaml из txt
+def dump_yaml (yamlfilename = 'cookbook.yaml'):
+	with open(yamlfilename, 'w', encoding='utf-8') as f:
+		txtcookbook = read_cookbook('cookbook.json')
+		yamlcookbook = yaml.dump(txtcookbook, f)
+
 def read_cookbook(cookbook_filename):
 	with open(cookbook_filename) as cookbook_file:
-		# по сути в одну строку just for fun
-		cookbook = {
-			line.strip(): [
-				dict(zip(['product','quantity','unit'], cookbook_file.readline().strip().split(' | '))) for _ in range(int(cookbook_file.readline()))
-			]
-			for line in cookbook_file if line.strip()
-		}
+		if cookbook_filename.split('.')[-1] == 'txt':
+			cookbook = {
+				line.strip(): [
+					dict(zip(['product','quantity','unit'], cookbook_file.readline().strip().split(' | '))) for _ in range(int(cookbook_file.readline()))
+				]
+				for line in cookbook_file if line.strip()
+			}
+		elif cookbook_filename.split('.')[-1] == 'json':
+			cookbook = json.load(cookbook_file)
+		elif cookbook_filename.split('.')[-1] == 'yaml':
+			cookbook = yaml.load(cookbook_file) 
 	return cookbook
 
 def get_menu(cookbook):
@@ -37,19 +56,19 @@ def print_shop_list(shop_list):
 		print('{product} {quantity} {unit}'.format(**shop_list_item))
 
 def create_shop_list():
-	menu = get_menu(read_cookbook('cookbook.txt'))
+	menu = get_menu(read_cookbook('cookbook.yaml'))
 	people_count = int(input('Сколько человек нужно накормить? '))
 	shop_list = get_shop_list_by_menu(menu, people_count)
 	print_shop_list(shop_list)
 
 def print_cookbook():
-	cookbook = read_cookbook('cookbook.txt')
+	cookbook = read_cookbook('cookbook.yaml')
 	print('Кулинарная книга:')
 	for key, cookbook_item in cookbook.items():
 		print(key)
 		print("".join(list(('{product} {quantity} {unit}\n'.format(**ingridient) for ingridient in cookbook_item))))
 
-functions = {'1': print_cookbook, '2': create_shop_list}
+functions = {'1': print_cookbook, '2': create_shop_list, '3': dump_json, '4': dump_yaml}
 while True:
 	user_input = input('Выберите действие:\n1 - Вывести кулинарную книгу.\n2 - Составить меню и список покупок.\nДля выхода введите любой другой символ.\n=> ')
 	functions.get(user_input, exit)()
