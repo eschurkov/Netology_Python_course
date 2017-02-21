7125d93b7a99fc325f8675b74128498feffe00ca
eschurkov <sch.egor@gmail.com>
2017-02-21 14:28:41 +0300

    v.0.1

--- Homework_4.1/names_stat.py
+++ Homework_4.1/names_stat.py
@@ -1,3 +1,19 @@
 import pandas
-data1 = pandas.read_csv('names/yob1990.txt')
-print(data1)
+import os
+source_directory = 'names'
+# years_list = [1880]
+years_list = [1900, 1950, 2000]
+
+data = pandas.DataFrame()
+for year in years_list:
+    files_path = os.path.join(source_directory, 'yob%s.txt' % str(year))
+
+    year_data = pandas.read_csv(files_path, names=['name', 'sex', 'count'])
+
+    data = data.append(year_data, ignore_index=True)
+
+data = data.groupby(by=['name', 'sex']).aggregate(sum)
+data = data.sort_values(by='count', ascending=False)[:3]
+
+print(data)
+
