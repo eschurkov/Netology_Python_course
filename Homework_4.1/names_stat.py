1338aa48df199e38a1c52b42efb46b37a7d73349
Egor Schurkov <sch.egor@gmail.com>
2017-02-21 20:22:54 +0300

    Задача 1 - done

--- Homework_4.1/names_stat.py
+++ Homework_4.1/names_stat.py
@@ -1,19 +1,22 @@
 import pandas
 import os
-source_directory = 'names'
-# years_list = [1880]
-years_list = [1900, 1950, 2000]
 
-data = pandas.DataFrame()
-for year in years_list:
-    files_path = os.path.join(source_directory, 'yob%s.txt' % str(year))
 
-    year_data = pandas.read_csv(files_path, names=['name', 'sex', 'count'])
+def top3_names(years_list):
+    source_directory = 'names'
+    data = pandas.DataFrame()
+    for year in years_list:
+        files_path = os.path.join(source_directory, 'yob%s.txt' % str(year))
 
-    data = data.append(year_data, ignore_index=True)
+        year_data = pandas.read_csv(files_path, names=['name', 'sex', 'count'])
 
-data = data.groupby(by=['name', 'sex']).aggregate(sum)
-data = data.sort_values(by='count', ascending=False)[:3]
+        data = data.append(year_data, ignore_index=True)
 
-print(data)
+    data = data.groupby(by=['name', 'sex']).aggregate(sum).reset_index()
+    data = data.sort_values(by='count', ascending=False)[:3]
 
+    print(list(data['name']))
+
+# years = [1880]
+years = [1900, 1950, 2000]
+top3_names(years)
